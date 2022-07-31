461fb2e3957a7d3feaa328943978541d6793f1c1
blueeagle <itberrios6@gmail.com>
2022-07-31 12:20:19 -0500

    updated utils and the starter

--- point_cloud_starter.py
+++ point_cloud_starter.py
@@ -13,10 +13,10 @@ import open3d as o3d
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from point_cloud_utils import get_clusters
+from point_cloud_utils import get_clusters_from_labels, get_knn_clusters
 
-## USE http://www.open3d.org/docs/release/tutorial/Basic/
 
+# =============================================================================
 ## 1 - open file and visualize the point cloud
 # The supported extension names are: pcd, ply, xyz, xyzrgb, xyzn, pts.
 # pcd_path = r'test_files\fragment.ply'
@@ -27,6 +27,7 @@ pcd = o3d.io.read_point_cloud(pcd_path)
 #import pptk
 #v = pptk.viewer(pcd.points)
 
+# =============================================================================
 ## 2 perform Voxel Grid Downsampling
 print(f"Points before downsampling: {len(pcd.points)} ")
 print("Downsample the point cloud with a voxel of 0.05")
@@ -36,6 +37,7 @@ pcd = pcd.voxel_down_sample(voxel_size=0.05)
 print(f"Points after downsampling: {len(pcd.points)}")# DOWNSAMPLING
 
 
+# =============================================================================
 ## 3 Segement ground plane from the rest of the point cloud
 tic = time.time()
 _, inliers = pcd.segment_plane(distance_threshold=0.25,
@@ -53,7 +55,7 @@ outlier_cloud.paint_uniform_color([1,0,1])
 # o3d.visualization.draw_geometries([inlier_cloud , outlier_cloud])
 # o3d.visualization.draw_geometries([outlier_cloud])
 
-
+# =============================================================================
 ## 4 - CLUSTERING USING DBSCAN
 outlier_cloud.paint_uniform_color([0,0,0])
 inlier_cloud.paint_uniform_color([0,0,0])
@@ -72,21 +74,96 @@ colors[labels < 0] = 0
 outlier_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
 o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
 
-# display indiviudal clusters
-cluster_clouds = get_clusters(outlier_cloud, colors, background_color=[0,0,0])
-o3d.visualization.draw_geometries(cluster_clouds)
+# get and display indiviudal clusters
+tic = time.time()
+clusters = get_clusters_from_labels(outlier_cloud, 
+                                    labels, 
+                                    background_color=[0,0,0])
+toc = time.time()
+print(f'time: {toc - tic}')
+o3d.visualization.draw_geometries(clusters)
     
-    
-    
-'''
 
+# =============================================================================
 ## BONUS CHALLENGE - CLUSTERING USING KDTREE AND KNN INSTEAD
 # http://www.open3d.org/docs/latest/tutorial/Basic/kdtree.html
-pcd_tree = 
+# pcd_tree = 
 
+# Recolor the point cloud 
+# outlier_cloud.paint_uniform_color([0, 0, 0])
+
+# Calculate the KDTree from the point cloud
+# pcd_tree = o3d.geometry.KDTreeFlann(outlier_cloud)
+
+# get K nearest neighbors
+# tic = time.time()
+# clusters = get_knn_clusters(outlier_cloud, pcd_tree, max_points=500, ep=2)
+# toc = time.time()
+# print(f'time: {toc - tic}')
+    
+# visualize clusters
+# o3d.visualization.draw_geometries(clusters)
+
+
+# =============================================================================
 ## CHALLENGE 5 - BOUNDING BOXES IN 3D
-bounding_boxes = 
+# bounding_boxes = 
 
+
+
+pcd_o3d = o3d.utility.Vector3dVector(np.asarray(outlier_cloud.points))
+bbox = o3d.geometry.OrientedBoundingBox.create_from_points(pcd_o3d)
+oriented_bbox = bbox.get_oriented_bounding_box()
+
+# draw bounding box
+# need to get bounding box corner points
+
+# helper array for speed hack!
+i_arr = np.array([
+    [1, 1, 1],
+    [1, 1, -1],
+    [1, -1, 1],
+    [1, -1, -1],
+    [-1, 1, 1],
+    [-1, 1, -1],
+    [-1, -1, 1],
+    [-1, -1,- 1]
+    ])
+
+points = oriented_bbox.center + oriented_bbox.extent*i_arr
+
+
+# then get lines?
+lines = [[0, 1], [0, 2], [1, 3], [2, 3], 
+         [4, 5], [4, 6], [5, 7], [6, 7],
+         [0, 4], [1, 5], [2, 6], [3, 7]]
+
+# then put them together
+line_set = o3d.geometry.LineSet()
+line_set.points = o3d.utility.Vector3dVector(points)
+line_set.lines = o3d.utility.Vector2iVector(lines)
+line_set.colors = o3d.utility.Vector3dVector(colors)
+
+# visualize
+o3d.visualization.draw_geometries([outlier_cloud, inlier_cloud, line_set])
+
+# then draw the box
+
+# TEMP
+points = [[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0], [0, 0, 1], [1, 0, 1],
+          [0, 1, 1], [1, 1, 1]]
+lines = [[0, 1], [0, 2], [1, 3], [2, 3], [4, 5], [4, 6], [5, 7], [6, 7],
+         [0, 4], [1, 5], [2, 6], [3, 7]]
+colors = [[1, 0, 0] for i in range(len(lines))]
+line_set = o3d.geometry.LineSet()
+line_set.points = o3d.utility.Vector3dVector(points)
+line_set.lines = o3d.utility.Vector2iVector(lines)
+line_set.colors = o3d.utility.Vector3dVector(colors)
+o3d.visualization.draw_geometries([line_set])
+    
+    
+
+'''
 ## CHALLENGE 6 - VISUALIZE THE FINAL RESULTS
 list_of_visuals = 
 
--- point_cloud_utils.py
+++ point_cloud_utils.py
@@ -8,126 +8,102 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def get_clusters(pcd, colors, background_color=[0,0,0]):
+
+def get_clusters_from_labels(pcd, labels, background_color=[0,0,0]):
     ''' Obtains a list of individual cluster point clouds and paints them 
-        unique colors. 
-        This function doesn;t actually get the clusters... it just gets the 
-        clusters based on their color. The new function will use labels to 
-        get the individual clusters in a list
+        unique colors. Assumes that the pcd object has a uniform color.
         Inputs:
             pcd - open3d PointCloud object
-            colors - (Nx3 array) Contains colors for each cluster
+            labels - (Nx1 array) labels for each point in the cluster
             background_color - (lust/array) original background color of pcd
         Outputs:
-            cluster_clouds (list) Contains PointCloud objecrts for each color
+            clusters (list) Contains PointCloud objects for each color
         '''
     # sanitize inputs
     if not isinstance(background_color, np.ndarray):
         background_color = np.array(background_color)
         
-    # each unique color value corresponds to a different cluster/category
-    clusters = colors[:, :3].sum(axis=1)
-
-    sorted_clusters = np.sort(clusters)
-    unique_colors = np.unique(colors[:, :3], axis=0)
-    
-    # this allows us to get the correct cluster RGB color
-    cluster_indexes = np.argsort(unique_colors.sum(axis=1))
-
-
-    # get cluster locations as a 2D list (or 2D array?)
-    cluster_clouds = []
-    for i, clust in enumerate(np.unique(sorted_clusters)):
-        
-        # get cluster color and point cloud locations
-        cluster_color = unique_colors[cluster_indexes[i], :3]
-        
-        # don't add unclustered points
-        if cluster_color.sum() == background_color.sum():
-            continue
-        
-        # get cluster point cloud 
-        cluster_loc = np.where(clusters == clust)[0].tolist()
-        
-        # get current cluster point cloud with color
-        cluster_cloud = pcd.select_by_index(cluster_loc)
-        cluster_cloud.paint_uniform_color(cluster_color)
-        
-        # append to list
-        cluster_clouds.append(cluster_cloud)
-        
-
-    return cluster_clouds
-    
-# new way
-def get_clusters_2(pcd, labels, background_color=[0,0,0]):
-    ''' Obtains a list of individual cluster point clouds and paints them 
-        unique colors
-        Inputs:
-            pcd - open3d PointCloud object
-            colors - (Nx1 array) labels for each point in the cluster
-            background_color - (lust/array) original background color of pcd
-        Outputs:
-            cluster_clouds (list) Contains PointCloud objecrts for each color
-        '''
-
     # get colors 
-    unique_labels = np.unique(labels)
     max_label = labels.max()
-    colors = plt.get_cmap('tab20')(unique_labels / (max_label if max_label > 0 else 1))
-    colors[unique_labels < 0] = 0
+    colors = plt.get_cmap('tab20')
+    colors = colors(labels/(max_label if max_label > 0 else 1))
+    colors[labels < 0] = 0
     
+    # get unique labels and remove -1 (unclustered label)
+    unique_labels = np.unique(labels)
+    unique_labels = unique_labels[unique_labels != -1]
     
-    # each unique color value corresponds to a different cluster/category
-    clusters = colors[:, :3].sum(axis=1)
-
-    sorted_clusters = np.sort(clusters)
-    unique_colors = np.unique(colors[:, :3], axis=0)
+    # store cluster point clouds in a list
+    clusters = []
     
-    # this allows us to get the correct cluster RGB color
-    cluster_indexes = np.argsort(unique_colors.sum(axis=1))
-
-
-    # get cluster locations as a 2D list (or 2D array?)
-    cluster_clouds = []
-    for i, clust in enumerate(np.unique(sorted_clusters)):
+    # iterate through each unique label
+    for label in unique_labels:
+        # get index of points and colors
+        cluster_idx = np.where(label == labels)[0]
         
-        # get cluster color and point cloud locations
-        cluster_color = unique_colors[cluster_indexes[i], :3]
+        # get cluster color
+        color = colors[cluster_idx, :3]
         
         # don't add unclustered points
-        if cluster_color.sum() == background_color.sum():
-            continue
+        # if color.sum() == background_color.sum():
+        #     continue
         
-        # get cluster point cloud 
-        cluster_loc = np.where(clusters == clust)[0].tolist()
+        # get cluster points
+        cluster = pcd.select_by_index(cluster_idx)
         
-        # get current cluster point cloud with color
-        cluster_cloud = pcd.select_by_index(cluster_loc)
-        cluster_cloud.paint_uniform_color(cluster_color)
+        # paint cluster
+        cluster.colors = o3d.utility.Vector3dVector(color)
         
         # append to list
-        cluster_clouds.append(cluster_cloud)
-        
-
-    return cluster_clouds
-
-
+        clusters.append(cluster)
     
-'''
-# Example for how to get clusters and colors
+    return clusters
 
 
-with o3d.utility.VerbosityContextManager(
-        o3d.utility.VerbosityLevel.Debug) as cm:
-    labels = np.array(outlier_cloud.cluster_dbscan(eps=0.5, 
-                                                   min_points=12, 
-                                                   print_progress=True))
-max_label = labels.max()
-print(f"point cloud has {max_label + 1} clusters")
+def get_knn_clusters(pcd, pcd_tree, max_points=500, ep=2):
+    ''' Obtains clusters using a KD Tree and K-Nearest Neighbors.
+        steps:
+            select a point at random
+            get it's K-nearest neighbors 
+            remove from search space all points within a set dist to the 
+                original point 
+            repeat
+        Inputs:
+        Outputs:
+        '''
+    
 
-# get cluster colors
-colors = plt.get_cmap('tab20')(labels / (max_label if max_label > 0 else 1))
-colors[labels < 0] = 0
+    # initialize the search space
+    points_arr = np.asarray(pcd.points)
+    search_space = np.arange(0, len(pcd.points))
 
-'''
+    # collect clusters in a list
+    clusters = []
+
+    for i in range(max_points):
+
+        # get a random point and color
+        rand_point = points_arr[np.random.randint(0, len(search_space)), :]
+        color = np.random.uniform(size=(3,))
+        
+        # get 300 nearest neighbors of the point
+        [k, idx, dist] = pcd_tree.search_knn_vector_3d(rand_point, 300)
+        
+        # only keep points within a distance of ep
+
+        idx_prune = np.array(idx)[np.array(dist) <= ep]
+         
+        # prune search space
+        search_space = np.setdiff1d(search_space, idx_prune)
+        
+        # collect cluster in list and paint it
+        cluster = pcd.select_by_index(idx_prune[1:])
+        cluster.paint_uniform_color(color)
+        
+        clusters.append(cluster)
+        
+        if len(search_space) < 20:
+            break
+        
+    return clusters
+    
